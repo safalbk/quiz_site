30e93e64f9c09cdea75e28d7a20f664525c64580
safalbk <safalbk333@gmail.com>
2023-05-29 11:47:24 +0530

    congig vercel

--- quiz_site/settings.py
+++ quiz_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "*wl&@!mr$lsao7z^f9$6z%v1gsc$w0l!8%g28_+c9&dg7j-5u-"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','*']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh','*']
 
 
 # Application definition
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
